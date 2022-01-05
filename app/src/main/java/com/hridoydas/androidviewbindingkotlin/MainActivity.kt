acab4429abbae002a17207864b1b31c9272b5517
Hridoy Das <>
2022-01-05 17:22:03 +0900

    Learn Add layout fragment with viewBinding

--- app/src/main/java/com/hridoydas/androidviewbindingkotlin/MainActivity.kt
+++ app/src/main/java/com/hridoydas/androidviewbindingkotlin/MainActivity.kt
@@ -2,10 +2,43 @@ package com.hridoydas.androidviewbindingkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.hridoydas.androidviewbindingkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Add ActivityMainBinding to inflate it to layoutFlater pass setContentview
+       val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       /* binding.textView.text="Hello I am Hridoy"
+        binding.textView2.text="Now Learning View binding"
+
+        binding.btnClickMe.setOnClickListener {
+            binding.textView.text = "Hello Now learn Time"
+            Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show()
+
+        }*/
+
+        with(binding){
+
+            textView.text="Hello I am Hridoy"
+            textView2.text="Now Learning View binding"
+
+
+
+            btnClickMe.setOnClickListener{
+                textView.text="Hello Now learn Time"
+                includedLayout.fragmentLayout.text="New Writing"
+               // Toast.makeText(this@MainActivity,"Hello World",Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
+
+
     }
 }
+
---
+++ app/src/main/java/com/hridoydas/androidviewbindingkotlin/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.hridoydas.androidviewbindingkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+
+
+}

0a0afe3927db7e938a24d7335cafa09f177ddd3c
Hridoy Das <>
2022-01-05 18:32:24 +0900

    Use view binding in fragments

--- app/src/main/java/com/hridoydas/androidviewbindingkotlin/MainFragment.kt
+++ app/src/main/java/com/hridoydas/androidviewbindingkotlin/MainFragment.kt
@@ -11,13 +11,27 @@ import java.time.Instant
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
+    private var _binding: FragmentMainBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedStateInstant: Bundle?): View? {
+  /*  override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedStateInstant: Bundle?): View? {
 
         val binding:FragmentMainBinding= FragmentMainBinding.inflate(inflater,container,false)
         binding.tvFragment.text="change change"
         return binding.root
+    }*/
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+       _binding = FragmentMainBinding.bind(view)
+        binding.tvFragment.text="change change"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
